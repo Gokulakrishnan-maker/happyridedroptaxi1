4189479c7e61196aea4298e677968d06d2546d8d
Gokulakrishnan-maker <gokulakrishnan756@gmail.com>
2025-10-01 16:42:42 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ const Header = () => {
               <img
                 src="/logo.png"
                 alt="Taxi Logo"
-                className="h-16 w-auto"
+                className="h-21 w-auto"
               />
               <span className="text-2xl font-bold text-gray-900"></span>
             </Link>
