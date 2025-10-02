d3fe403a6b0d5da3d52d380bff6ce545c4bc0a4c
Gokulakrishnan-maker <gokulakrishnan756@gmail.com>
2025-10-02 12:52:27 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
               <img
                src="/logo.png"
                alt="HappyrideDropTaxi Logo"
-               className="h-29 md:h-30 w-auto object-contain"
+               className="h-29 md:h-24 w-auto object-contain"
                />
               </div>
               <span className="text-2xl font-bold text-gray-900"></span>
