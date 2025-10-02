f2bebc73135ea930bb18a3ebdd8ae4ec5a539dfc
Gokulakrishnan-maker <gokulakrishnan756@gmail.com>
2025-10-02 11:23:54 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,11 +28,7 @@ const Header = () => {
           >
            <Link to="/" className="flex items-center space-x-3">
            <span className="flex items-center">
-           <img
-           src="/logo.png"
-           alt="HappyrideDropTaxi Logo"
-           className="h-20 w-auto md:h-24" // ✅ Bigger responsive size
-           />
+           
           <span className="ml-2 text-3xl md:text-4xl font-extrabold">
           <span className="text-yellow-500">Happyride</span>
           <span className="text-black">DropTaxi</span>
